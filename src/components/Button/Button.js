c8d884eea814f46894a1c3fcb870f5228aa2cfb0
VincentHChoy <vhchoy@ualberta.ca>
2022-10-19 15:54:57 -0600

    adds button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,8 +1,14 @@
-function Button({ style, type = 'tooltip', handleClick, text }) {
-
+function Button({ style, type = "tooltip", handleClick, text }) {
   return (
-    <button style={style} type={type} className="text-primary border-2 border-primary rounded-md py-5 px-10 hover:bg-primary hover:text-white" onClick={handleClick}>{text}</button>
-  )
+    <button
+      style={style}
+      type={type}
+      className="text-primary border-2 border-primary rounded-md py-3 px-12 hover:bg-primary hover:text-white"
+      onClick={handleClick}
+    >
+      {text}
+    </button>
+  );
 }
 
-export default Button
+export default Button;
