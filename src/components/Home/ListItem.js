5597b0351cfaf912b32f4f429aea64b1427fb69e
VincentHChoy <vhchoy@ualberta.ca>
2022-10-20 15:59:50 -0600

    modfies styling of list item

--- src/components/Home/ListItem.js
+++ src/components/Home/ListItem.js
@@ -3,7 +3,7 @@ import React from "react";
 function ListItem(props) {
   return (
     <li className="flex flex-row items-center justify-start font-comfortaa">
-      <div className="m-4 text-3xl font-bold">{props.grade}</div>
+      <div className="text-3xl font-bold w-16">{props.grade}</div>
       <main className="flex flex-col text-left m-4">
         <h1 className="font-bold text-2xl text-left">{props.name}</h1>
         <h2 className="text-base">{props.location}</h2>
