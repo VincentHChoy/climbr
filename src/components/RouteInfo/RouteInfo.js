cafa996c3514e866fe2f11f7b358352d5e887c25
VincentHChoy <vhchoy@ualberta.ca>
2022-10-22 19:14:37 -0600

    changes from static page to dynamic page

--- src/components/RouteInfo/RouteInfo.js
+++ src/components/RouteInfo/RouteInfo.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaHammer } from "react-icons/fa";
 import { BsLightning, BsLightningFill } from "react-icons/bs";
 import { AiFillStar, AiOutlineStar } from "react-icons/ai";
@@ -10,22 +10,24 @@ import {
 import { IoIosArrowBack } from "react-icons/io";
 
 function RouteInfo(props) {
+  const [completed, setCompleted] = useState("Not Yet");
+  const img = props.img !== "null" ? props.img : "https://t4.ftcdn.net/jpg/04/70/29/97/360_F_470299797_UD0eoVMMSUbHCcNJCdv2t8B2g1GVqYgs.jpg"
+
   return (
-    <main className="flex flex-col text-left m-4">
+    <main className="flex flex-col text-left m-4 overflow-hidden">
       <section className="flex flex-row">
         <IoIosArrowBack size={32} className={"text-secondary cursor-pointer"} />
         <span className="mx-5 text-left w-5/6">
           <h1 className="font-bold text-2xl text-left cursor-pointer hover:underline">
             {props.name}
-            {"Easy in Easy Chair"}
           </h1>
-          <h2 className="text-base">{"Easy Chair"}</h2>
+          <h2 className="text-base">{props.location}</h2>
         </span>
       </section>
       <section className="flex flex-row my-6 items-center justify-start">
         <span className="mx-5 text-left">
           <h2 className="font-bold text-base text-left">{"Grade"}</h2>
-          <h1 className="text-3xl font-bold">{"V4"}</h1>
+          <h1 className="text-3xl font-bold">{props.grade}</h1>
         </span>
         <div className="flex flex-row">
           <ImCheckmark2 size={32} className={"text-green-500 cursor-pointer"} />
@@ -42,13 +44,18 @@ function RouteInfo(props) {
       </section>
       <section className="mx-5">
         <h1 className="font-bold">Description</h1>
-        <br/>
-        <p>Start low and traverse right until it is possible to mantle. Slopes and hooks are abundant here.</p>
         <br />
-        <img className=" rounded-2xl" src="https://image.thecrag.com/427x320/58/e7/58e76bd52cdcda2783526e12cbf427e92b1cd266"/>
+        <p className="w-2/3 break-normal">
+          {props.desc}
+        </p>
+        <br />
+        <img
+          className=" rounded-2xl"
+          src={img} 
+        />
       </section>
-      <br/>
-      <h1 className="font-bold">completed on: October 22nd 2022</h1>
+      <br />
+      <h1 className="font-bold">completed on: {completed}</h1>
     </main>
   );
 }
