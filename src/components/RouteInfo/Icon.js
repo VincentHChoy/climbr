b2bc4f8429be542d72f5cea5ac1f82d58735a044
VincentHChoy <vhchoy@ualberta.ca>
2022-10-24 15:46:59 -0600

    removes console log

--- src/components/RouteInfo/Icon.js
+++ src/components/RouteInfo/Icon.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 
 function Icon(props) {
   const [openIcon, setOpenIcon] = useState(props.action === "true")
-  console.log(props.action);
   const icon = openIcon ? props.icon2 : props.icon1
 
   return (
