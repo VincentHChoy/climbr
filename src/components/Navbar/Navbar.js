9b826a8e4c38b814b28aa0a7c16cf51432248c89
VincentHChoy <vhchoy@ualberta.ca>
2022-10-25 16:35:48 -0600

    update navbar with react router

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,47 +1,52 @@
 import React, { useState } from "react";
 import { BsHouse, BsSearch } from "react-icons/bs";
 import { IoStatsChart } from "react-icons/io5";
+import { Link, useParams, useLocation } from "react-router-dom";
 
 function Navbar() {
+  const { pathname } = useLocation();
   const [selected, setSelected] = useState("home");
   const selectClass = "box-border rounded-full bg-white";
   const highlightHome =
-    selected === "home" ? selectClass : "text-white cursor-pointer";
+    pathname === "/home" ? selectClass : "text-white cursor-pointer";
   const highlightSearch =
-    selected === "search" ? selectClass : "text-white cursor-pointer";
+    pathname === "/search" ? selectClass : "text-white cursor-pointer";
   const highlightStats =
-    selected === "stats" ? selectClass : "text-white cursor-pointer";
+    pathname === "/stats" ? selectClass : "text-white cursor-pointer";
 
   return (
     <main className="fixed bottom-0 bg-secondary w-screen h-20 rounded">
       <section className="flex flex-row justify-around my-2">
-        <div className={`flex justify-center items-center w-20 h-14 ${highlightSearch}`}>
-          <BsSearch
-            size={36}
-            className={``}
+        <Link to="/search">
+          <div
+            className={`flex justify-center items-center w-20 h-14 ${highlightSearch}`}
             onClick={() => {
               setSelected("search");
             }}
-          />
-        </div>
-        <div className={`flex justify-center items-center w-20 h-14 ${highlightHome}`}>
-          <BsHouse
-            size={36}
-            className={``}
+          >
+            <BsSearch size={36} />
+          </div>
+        </Link>
+        <Link to="/home">
+          <div
+            className={`flex justify-center items-center w-20 h-14 ${highlightHome}`}
             onClick={() => {
               setSelected("home");
             }}
-          />
-        </div >
-        <div className={`flex justify-center items-center w-20 h-14 text ${highlightStats}`}>
-          <IoStatsChart
-            size={36}
-            className={``}
+          >
+            <BsHouse size={36} />
+          </div>
+        </Link>
+        <Link to="/stats">
+          <div
+            className={`flex justify-center items-center w-20 h-14 text ${highlightStats}`}
             onClick={() => {
               setSelected("stats");
             }}
-          />
-        </div>
+          >
+            <IoStatsChart size={36} />
+          </div>
+        </Link>
       </section>
     </main>
   );
