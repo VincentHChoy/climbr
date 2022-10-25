143cfa3ce1e1b6389417308a57f24fa97e81031e
VincentHChoy <vhchoy@ualberta.ca>
2022-10-25 15:52:04 -0600

    adds error styling for button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,9 @@
-function Button({ style, type = "tooltip", handleClick, text }) {
+function Button({ style, type = "tooltip", handleClick, text,color }) {
   return (
     <button
       style={style}
       type={type}
-      className="text-primary border-2 border-primary rounded-md py-3 px-12 hover:bg-primary hover:text-white"
+      className={`text-primary border-2 border-primary rounded-md py-3 px-12 hover:bg-primary hover:text-white ${color}`}
       onClick={handleClick}
     >
       {text}
