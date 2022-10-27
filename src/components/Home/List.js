cc3538a80c5669c236fde4e9cf178ff7a74c8d84
VincentHChoy <vhchoy@ualberta.ca>
2022-10-27 13:13:42 -0600

    filters data and gets most up to date achievement

--- src/components/Home/List.js
+++ src/components/Home/List.js
@@ -1,22 +1,51 @@
-import React, { useState } from "react";
+import React from "react";
 import ListItem from "./ListItem";
 
 function List(props) {
-  const list = props.list.map((route) => {
-    // console.log("route",route);
+  const filterMistakes = props.list.filter(
+    (route) => route.completedOn !== "not yet"
+  );
+
+  const removeDuplicates = () => {
+    const memo = {};
+    const result = [];
+    filterMistakes.map((route) => {
+      memo[route.routeName] = false;
+    });
+
+    filterMistakes.map((route) => {
+      if (memo[route.routeName] === false) {
+        memo[route.routeName] = true;
+        result.push(route);
+      }
+    });;
+    return result;
+  };
+
+  const uniqueList = removeDuplicates();
+  console.log(uniqueList);
+
+  const sortedResult = uniqueList.sort(
+    (a, b) => b.difficulty[1] - a.difficulty[1]
+  );
+
+  const result = sortedResult;
+
+  const list = result.map((route) => {
     return (
       <ListItem
         name={route.routeName}
         location={route.location}
         grade={route.difficulty}
         flashed={route.flashed}
-        completed = {route.completedOn}
+        completed={route.completedOn}
       />
     );
   });
 
+
   return (
-    <main className="flex flex-col h-1/2 overflow-auto relative mb-10">
+    <main className="flex flex-col h-1/2 overflow-auto relative mb-32">
       {<ul>{list}</ul>}
     </main>
   );
