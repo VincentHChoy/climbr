226b6d275a96822dd6c928ff25b40cb7519fc91b
VincentHChoy <vhchoy@ualberta.ca>
2022-10-26 21:02:15 -0600

    new react router routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import "./App.css";
 import Button from "./components/Button/Button";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="h-screen">
-    <main className="flex flex-col items-center justify-center h-screen">
+      <main className="flex flex-col items-center justify-center h-screen">
         <img className="h-56 -my-10" src="mountain.png"></img>
-        <h1 className="font-comfortaa text-black text-5xl font-bold">
-          Climbr
-        </h1>
+        <h1 className="font-comfortaa text-black text-5xl font-bold">Climbr</h1>
         <p className="font-comfortaa text-xl text-center my-3 mx-6 w-72">
           A journal for logging your Squamish climbing journey
         </p>
-        <Button text={"Next"} />
-    </main>
+        <Link to="/reg">
+          <Button text={"Next"} />
+        </Link>
+      </main>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,18 +5,22 @@ import App from "./App";
 import Home from "./components/Home/Home";
 import Search from "./components/Search/Search";
 import Stats from "./components/Stats/Stats";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Register from "./components/Register/Register";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<Home />}/>
+        <Route path="/" element={<App/>} />
+        <Route path="/reg" element={<Register />} />
         <Route path="/home" element={<Home />} />
         <Route path="/search" element={<Search />} />
         <Route path="/stats" element={<Stats />} />
       </Routes>
     </Router>
+
   </React.StrictMode>
 );
