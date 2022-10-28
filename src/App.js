7c67c33f2b34084f2e9c3091a1437eecbac4ada9
VincentHChoy <vhchoy@ualberta.ca>
2022-10-27 22:33:38 -0600

    add github link on landing page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Button from "./components/Button/Button";
+import { AiFillGithub } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { AiFillDownCircle } from "react-icons/ai";
 import Fade from 'react-reveal/Fade';
@@ -9,6 +10,11 @@ function App() {
     "flex flex-col md:flex-row items-center justify-around p-10 bg-[#D6DEFF] mt-1/2";
   return (
     <div className="h-screen bg-[#EEF3FC]">
+
+      <a href="https://github.com/VincentHChoy/climbr" target='_blank'>
+        <AiFillGithub size={50} className="text-secondary fixed top-0 right-0 opacity-80 cursor-pointer" />
+      </a>
+
       <main className="flex flex-col items-center justify-center h-5/6">
         <img className="md:h-4/6 -my-10" src="mountain.png"/>
         <h1 className="font-comfortaa text-black md:text-9xl text-6xl font-bold">Climbr</h1>
