35dac924ae3cdeb9e0013e23986bae837f4c2dc1
VincentHChoy <vhchoy@ualberta.ca>
2022-10-27 22:00:22 -0600

    landing page full refactor

--- src/App.js
+++ src/App.js
@@ -1,20 +1,72 @@
 import "./App.css";
 import Button from "./components/Button/Button";
 import { Link } from "react-router-dom";
+import { AiFillDownCircle } from "react-icons/ai";
+import Fade from 'react-reveal/Fade';
 
 function App() {
+  const sectionStyle =
+    "flex flex-col md:flex-row items-center justify-around p-10 bg-[#D6DEFF] mt-1/2";
   return (
-    <div className="h-screen">
-      <main className="flex flex-col items-center justify-center h-screen">
-        <img className="h-56 -my-10" src="mountain.png"></img>
-        <h1 className="font-comfortaa text-black text-5xl font-bold">Climbr</h1>
-        <p className="font-comfortaa text-xl text-center my-3 mx-6 w-72">
+    <div className="h-screen bg-[#EEF3FC]">
+      <main className="flex flex-col items-center justify-center h-5/6">
+        <img className="md:h-4/6 -my-10" src="mountain.png"/>
+        <h1 className="font-comfortaa text-black md:text-9xl text-6xl font-bold">Climbr</h1>
+        <h1 className="font-comfortaa text-3xl text-center my-3 mx-6 md:w-1/3">
           A journal for logging your Squamish climbing journey
-        </p>
+        </h1>
+
         <Link to="/reg">
           <Button text={"Next"} />
         </Link>
       </main>
+        <AiFillDownCircle size={50} className="text-primary animate-bounce h-1/6 mx-auto" />
+      <section className={`${sectionStyle}`}>
+        <Fade left>
+        <img className="rounded-2xl my-10" src="achivements.png"/>
+        <h1 className="text-center text-4xl break-words">
+          See all your sends and flashes for Squamish's top 100 boulders in one place
+        </h1>
+        </Fade>
+      </section>
+      <section className={`md:flex-row-reverse ${sectionStyle}`}>
+        <Fade right>
+        <img className="md:w-1/2 rounded-2xl my-10" src="search.png"/>
+        <h1 className="text-center text-4xl break-words mx-5">
+            Filter, Search and log your <bold className="text-green-500">sends</bold>, <bold className="text-yellow-500">flashes</bold>, <bold className="text-orange-500">projects</bold>, and <bold className="text-primary">favorites</bold>
+        </h1>
+        </Fade>
+      </section>
+
+      <section className={sectionStyle}>
+        <Fade left>
+        <img className="md:w-1/2 rounded-2xl my-10" src="analytics.png"></img>
+        <h1 className="text-center text-4xl break-words">
+          Log your progress and see your{" "}
+          <bold className="text-secondary">analytics</bold>
+        </h1>
+        </Fade>
+      </section>
+
+      <section className={`md:flex-row-reverse ${sectionStyle}`}>
+        <Fade right>
+        <img className="md:w-1/2 rounded-2xl my-10" src="mockup.png"></img>
+        <h1 className="text-center text-4xl break-words">
+          Mobile and Desktop Compatible
+        </h1>
+        </Fade>
+      </section>
+
+      <section className={`flex flex-col md:flex-col ${sectionStyle}`}>
+        <Fade down>
+        <h1 className="text-center text-4xl break-words py-10">
+          Lets Get Climbing!
+        </h1>
+        <Link to="/reg">
+          <Button text={"Start"} />
+        </Link>
+        </Fade>
+      </section>
     </div>
   );
 }
