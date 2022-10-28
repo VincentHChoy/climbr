652929e931d7d73a4c53f0dabae6da8e75ee2570
VincentHChoy <vhchoy@ualberta.ca>
2022-10-27 21:20:34 -0600

    adds new animals

--- src/components/ProfilePicture/Picture.js
+++ src/components/ProfilePicture/Picture.js
@@ -2,32 +2,37 @@ import React, { useEffect, useState } from "react";
 import { BsDice6 } from "react-icons/bs";
 
 function Picture() {
-  const [picture, setPicture] = useState(localStorage.getItem("pfp"))
-  const currentName = localStorage.getItem("name") || "";
+  const [picture, setPicture] = useState(localStorage.getItem("pfp"));
+  const currentName = localStorage.getItem("name");
 
   const randomPicture = () => {
-    const pictures = ["bird.png", "jraff.png", "capy.webp", "dog.png", "cat.jpg"];
+    const pictures = ["bear.png", "bird.png","bird2.png","cat.jpg","fox.png","giraffe.png","goat.png","gorilla.png",
+  "hippo.png","koala.png","monkey.png"];
     const number = Math.floor(Math.random() * pictures.length);
 
-    setPicture(pictures[number])
+    setPicture(pictures[number]);
   };
 
   useEffect(() => {
     localStorage.setItem("pfp", picture);
-  }, [picture])
-  
+  }, [picture]);
+
   return (
     <main className="flex flex-row justify-center items-start">
       <section className="flex flex-col justify-center items-center my-5">
-        <img className="rounded-full w-64 h-64" src={picture} />
+        <img className="rounded-full w-64 h-64" src={picture || "cat.jpg"} />
         <h2 className="text-3xl my-2 font-comfortaa font-bold">
           {currentName}
         </h2>
       </section>
       <section className="flex items-start">
-        <BsDice6 size={24} className="hover:text-primary cursor-pointer my-4" onClick={()=>{
-          randomPicture();
-        }} />
+        <BsDice6
+          size={24}
+          className="hover:text-primary cursor-pointer my-4"
+          onClick={() => {
+            randomPicture();
+          }}
+        />
       </section>
     </main>
   );
